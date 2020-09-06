f6ce5582cec5ab47d16f2e18e285bcad589a7089
Marci <matusekma@gmail.com>
2020-09-06 20:13:33 +0200

    Add url to jura-fragen and make filename contain timestamp

--- webscraping/scrape_jura_fragen.py
+++ webscraping/scrape_jura_fragen.py
@@ -2,6 +2,7 @@ import json
 
 from parsing.get_soup import get_soup
 from parsing.extract_qa import extract_qa_jura_fragen
+from utils import current_time_millisec
 
 
 def scrape_topic_pages(topic):
@@ -18,8 +19,9 @@ def scrape_topic_pages(topic):
         for article_title in articles:
             article_url = article_title.a["href"]
             article_html = get_soup(article_url)
-            question_answer_tags = extract_qa_jura_fragen(article_html)
-            questions_and_answers.append(question_answer_tags)
+            article_data = extract_qa_jura_fragen(article_html)
+            article_data["url"] = article_url
+            questions_and_answers.append(article_data)
         page += 1
 
     return questions_and_answers
@@ -35,7 +37,7 @@ for topic in topics:
 json_string = json.dumps(qa)
 json_string_pretty = json.dumps(qa, indent=4, separators=(",", ": "))
 
-with open('./data/jura_fragen.json', 'w') as outfile:
+with open(f'./data/jura_fragen_{current_time_millisec()}.json', 'w') as outfile:
     json.dump(json_string, outfile)
 
 print(json_string_pretty)
